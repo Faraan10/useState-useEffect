83a584ab5f415fb6e03149ad02f2dcf8367d3976
Muhammed <faraanshaik19@gmail.com>
2025-05-18 23:23:57 +0530

    packages commit

---
+++ src/pages/index.js
@@ -0,0 +1,5 @@
+export { default as Home } from "./Home";
+export { default as Dashboard } from "./DashBoard";
+export { default as login } from "./Login";
+export { default as Register } from "./Register";
+export { default as NotFound } from "./NotFound";
