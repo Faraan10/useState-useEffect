f4a70f8802f2d6456733db9198a0c6afe404c070
Muhammed <faraanshaik19@gmail.com>
2025-05-18 23:24:22 +0530

    app commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 const App = () => {
   return (
     <>
-      <h1 class="text-3xl font-bold underline">Hello world!</h1>{" "}
+      <h1 class="text-3xl font-bold underline">Hello world!</h1>
     </>
   );
 };
