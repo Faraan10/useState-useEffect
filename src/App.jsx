93ee41837f60a1e2d3a2b475e08ebaaa68d9c068
Muhammed <faraanshaik19@gmail.com>
2025-05-19 23:38:03 +0530

    app updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Home, Dashboard, Register, Login, NotFound } from "./pages/index";
+import AuthLayout from "./layouts/AuthLayout";
+import MainLayout from "./layouts/MainLayout";
 
 const App = () => {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route element={<MainLayout />}>
+            <Route path="/" element={<Home />} />
+            <Route path="*" element={<NotFound />} />
+          </Route>
+
+          <Route element={<AuthLayout />}>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 export { default as Home } from "./Home";
 export { default as Dashboard } from "./DashBoard";
-export { default as login } from "./Login";
+export { default as Login } from "./Login";
 export { default as Register } from "./Register";
 export { default as NotFound } from "./NotFound";
