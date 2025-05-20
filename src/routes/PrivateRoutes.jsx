d37d98cad91520ee4bb571aea48d70b166cf8c31
Muhammed <faraanshaik19@gmail.com>
2025-05-20 21:21:30 +0530

    private route updated

--- src/routes/PrivateRoutes.jsx
+++ src/routes/PrivateRoutes.jsx
@@ -1,7 +1,13 @@
-import React from "react";
+import { Outlet } from "react-router-dom";
 
 const PrivateRoutes = () => {
-  return <div>PrivateRoutes</div>;
+  return (
+    <>
+      <main>
+        <Outlet />
+      </main>
+    </>
+  );
 };
 
 export default PrivateRoutes;
