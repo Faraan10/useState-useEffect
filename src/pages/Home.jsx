64e70caf3422be6ad0294d49df24731f22c84f2a
Muhammed <faraanshaik19@gmail.com>
2025-05-30 22:33:46 +0530

    home commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <div className="container">
+        <div className="flexbox">
+          <h2 className="text-2xl">
+            Items are just one click away to be at your doorstep
+          </h2>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Home;
