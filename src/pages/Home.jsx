a96dc9f96eec801a1103666778160b2488fe5de7
Muhammed <faraanshaik19@gmail.com>
2025-05-30 22:40:59 +0530

    app commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,9 +5,10 @@ const Home = () => {
     <>
       <div className="container">
         <div className="flexbox">
-          <h2 className="text-2xl">
+          <h2 className="text-2xl p-4">
             Items are just one click away to be at your doorstep
           </h2>
+          <button className="btn btn-primary p-4 m-4">Shop Now</button>
         </div>
       </div>
     </>
