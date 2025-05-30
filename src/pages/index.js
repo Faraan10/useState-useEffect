ab67427a245ec011907b08e06b0fbcd579ddb49c
Muhammed <faraanshaik19@gmail.com>
2025-05-30 22:50:14 +0530

    index commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,4 +2,6 @@ export { default as Home } from "./Home";
 export { default as Dashboard } from "./DashBoard";
 export { default as Login } from "./Login";
 export { default as Register } from "./Register";
+export { default as Products } from "./Products";
+export { default as Cart } from "./Cart";
 export { default as NotFound } from "./NotFound";
