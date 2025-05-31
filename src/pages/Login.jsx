4c34dcf4dc1159b3b884a0fb08c15f093c3ea125
Muhammed <faraanshaik19@gmail.com>
2025-05-31 22:58:56 +0530

    logi commit

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <>
+      <div>
+        <h1>Login below</h1>
+        <input type="text" placeholder="name" />
+        <input type="email" placeholder="enter email" />
+      </div>
+    </>
+  );
 };
 
 export default Login;
