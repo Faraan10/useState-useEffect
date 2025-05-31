e9599b7ceabad9119cd30dfeade31ceab99f4da6
Muhammed <faraanshaik19@gmail.com>
2025-05-31 23:02:42 +0530

    register commit

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -5,8 +5,12 @@ const Register = () => {
     <>
       <div>
         <h1>Register below</h1>
-        <input type="text" placeholder="name" />
+        <input type="text" placeholder="enter name" />
         <input type="email" placeholder="enter email" />
+        <input type="password" placeholder="enter password" />
+        <input type="password" placeholder="re-enter password" />
+
+        <input type="submit" className="btn btn-primary" value="submit" />
       </div>
     </>
   );
