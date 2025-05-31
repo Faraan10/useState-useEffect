ef559358c15c2f18969fc96e0295e0364585461e
Muhammed <faraanshaik19@gmail.com>
2025-05-31 22:58:32 +0530

    register commit

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const Register = () => {
-  return <div>Register</div>;
+  return (
+    <>
+      <div>
+        <h1>Register below</h1>
+        <input type="text" placeholder="name" />
+        <input type="email" placeholder="enter email" />
+      </div>
+    </>
+  );
 };
 
 export default Register;
