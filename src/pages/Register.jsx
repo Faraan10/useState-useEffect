4b9472cd3a2f11fbf5d45b5b3516b56306eee939
Muhammed <faraanshaik19@gmail.com>
2025-06-01 21:32:50 +0530

    register commit

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -44,6 +44,17 @@ const Register = () => {
             />
           </div>
 
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Confirm Password</span>
+            </label>
+            <input
+              type="password"
+              placeholder="Re-enter password"
+              className="input input-bordered w-full"
+              required
+            />
+          </div>
           <div className="form-control mt-6">
             <button className="btn btn-primary w-full">Register</button>
           </div>
