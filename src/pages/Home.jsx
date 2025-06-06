ddf56792b2eefed60a821fb20b14f4d711e097f2
Muhammed <faraanshaik19@gmail.com>
2025-06-06 21:57:57 +0530

    Home commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 const Home = () => {
   return (
     <div className="min-h-screen flex items-center justify-center bg-base-200">
@@ -5,7 +7,9 @@ const Home = () => {
         <h2 className="text-3xl font-bold text-primary mb-6">
           Items are just one click away to be at your doorstep
         </h2>
-        <button className="btn btn-primary btn-lg">Shop Now</button>
+        <button className="btn btn-primary btn-lg">
+          <Link to="/products">Shop Now</Link>
+        </button>
       </div>
     </div>
   );
