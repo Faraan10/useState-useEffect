52da15b9bb16c8e3b51976a2eb49d76c4f454ee0
Muhammed <faraanshaik19@gmail.com>
2025-06-09 00:38:40 +0530

    mainlayout commit

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,10 +1,10 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
 
-const MainLayout = () => {
+const MainLayout = ({ items }) => {
   return (
     <>
-      <Navbar />
+      <Navbar items={items} />
       <main>
         <Outlet />
       </main>
