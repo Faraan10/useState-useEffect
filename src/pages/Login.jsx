0543c538cde626388f99c5275aac783862f7f0f1
Muhammed <faraanshaik19@gmail.com>
2025-06-18 14:08:34 +0530

    login commit

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -10,14 +10,18 @@ const Login = () => {
   const handleChange = (e) => {
     setData({ ...data, [e.target.name]: e.target.value });
   };
-  console.log(data);
+  // console.log(data);
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // console.log(data);
+  };
   const { email, password } = data;
   return (
     <div className="min-h-screen flex items-center justify-center bg-base-200">
       <div className="w-full max-w-md p-8 space-y-6 bg-base-100 rounded-2xl shadow-lg">
         <h2 className="text-3xl font-bold text-center text-primary">Login</h2>
-        <form className="space-y-4">
+        <form className="space-y-4" onSubmit={handleSubmit}>
           <div className="form-control">
             <label className="label">
               <span className="label-text">Email</span>
