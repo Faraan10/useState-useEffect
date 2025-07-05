39efc4756d2aa13bd2fcd6dff3f19678d6b5fcf9
Muhammed <faraanshaik19@gmail.com>
2025-07-05 23:36:29 +0530

    products commit

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,37 +1,26 @@
-import React from "react";
-
 const Products = ({ items, addItems }) => {
   return (
-    <div className="min-h-screen bg-base-200 py-10 px-4">
-      <h2 className="text-3xl font-bold text-center mb-8">Our Products</h2>
-
-      <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 max-w-6xl mx-auto">
-        {items.map((product) => (
-          <div key={product.id} className="card bg-base-100 shadow-xl">
-            <figure>
-              <img
-                src={product.image}
-                alt={product.name}
-                className="h-48 w-40"
-              />
-            </figure>
-            <div className="card-body">
-              <h3 className="card-title">{product.name}</h3>
-              <div className="card-actions justify-end">
-                <p className="text-base">${product.price.toFixed(2)}</p>
-
-                <button
-                  className="btn btn-primary btn-sm"
-                  onClick={() => addItems(product)}
-                >
-                  Add to Cart
-                </button>
+    <>
+      <div className="min-h-screen bg-base-300 py-10 px-4">
+        <h2 className="text-3xl font-bold text-center mb-8">Our Products</h2>
+        <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 max-w-6xl mx-auto">
+          {items.map((item) => (
+            <div key={item.id} className="card bg-base-100 shadow-xl">
+              <figure>
+                <img
+                  src={item.image}
+                  alt={item.name}
+                  className="h-50 w-45 m-4"
+                />
+              </figure>
+              <div className="card-body">
+                <h4 className="card-title">{item.name}</h4>
               </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
