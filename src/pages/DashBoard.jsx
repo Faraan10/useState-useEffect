476de9d546977adbfc970917d62cb6c9872955c2
Muhammed <faraanshaik19@gmail.com>
2025-07-06 23:21:20 +0530

    dashboard commit

--- src/pages/DashBoard.jsx
+++ src/pages/DashBoard.jsx
@@ -36,7 +36,7 @@ const DashBoard = () => {
         </ul>
       </div>
 
-      {/* Main Content */}
+      {/* Main Content below*/}
       <div className="flex-1 p-6">
         <h1 className="text-3xl font-bold mb-4">Welcome to your Dashboard</h1>
 
